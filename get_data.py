f809282382cf6dea07d6167cfec58459923fe9e0
Victor Ribeiro <victorribeiro@teksystem.com.br>
2019-11-12 07:33:23 -0300

    refactor main.py

---
+++ get_data.py
@@ -0,0 +1,20 @@
+import json
+import requests
+import time
+
+
+categories = ["newstories", "showstories", "topstories", "beststories"]
+
+for category in categories:
+
+	stories_ids = requests.get("https://hacker-news.firebaseio.com/v0/{}.json".format(category)).json()
+
+	for storie in stories_ids:
+		result = requests.get("https://hacker-news.firebaseio.com/v0/item/{}.json".format(storie)).json()
+		data.append( result )
+		time.sleep(.5)
+		
+	with open('{}.json'.format(category), 'w') as f:
+		f.write( json.dumps( data ) )
+
+	print("{} done!".format(category))
